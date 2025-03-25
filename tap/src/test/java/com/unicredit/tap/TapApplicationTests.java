cb8b0f7299c85bdd65f2ab740466909218282b7d
IvanMelaz <ilmelaz@yahoo.it>
2025-03-25 18:47:15 +0100

    First Commit of process , end task CSV reader

--- tap/src/test/java/com/unicredit/tap/TapApplicationTests.java
+++ tap/src/test/java/com/unicredit/tap/TapApplicationTests.java
@@ -1,13 +1,33 @@
 package com.unicredit.tap;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
+@ContextConfiguration(locations = "classpath:batch-config.xml") // Adjust path if needed.
 class TapApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private JobLauncher jobLauncher;
+
+	@Autowired
+	private Job job; // Assuming your job bean is named 'job' in batch-config.xml
+
+/*	@Test
+	void testJobWithInputPathParameterWrongPath() {
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addString("nas.input.path", "nonexistent/input/file.csv")
+				.toJobParameters();
+
+		assertDoesNotThrow(() -> jobLauncher.run(job, jobParameters));
+	}*/
 
 }
