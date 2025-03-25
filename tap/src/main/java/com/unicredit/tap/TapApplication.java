161bf56202d8fdd56df8f58a98a1256c23d37c70
IvanMelaz <ilmelaz@yahoo.it>
2025-03-25 18:47:33 +0100

    First Commit of process , end task CSV reader

---
+++ tap/src/main/java/com/unicredit/tap/TapApplication.java
@@ -0,0 +1,15 @@
+package com.unicredit.tap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:batch-config.xml") // Import your XML configuration
+public class TapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapApplication.class, args);
+	}
+
+}
